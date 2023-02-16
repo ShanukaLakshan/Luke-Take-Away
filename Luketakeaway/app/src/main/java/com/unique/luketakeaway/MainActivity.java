207323cc6cd09337fb1d5938f5050b4f0bc7c3fd
ShanukaLakshan <lakshants20.uom.lk>
2023-02-16 19:07:29 +0530

    Create Splash Screen, Login, Register

---
+++ Luketakeaway/app/src/main/java/com/unique/luketakeaway/MainActivity.java
@@ -0,0 +1,26 @@
+package com.unique.luketakeaway;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        },5000);
+
+    }
+}
